c388c3945c653516c9e79cb361864f27185a9167
Tusck <36819587+Tusck@users.noreply.github.com>
2018-04-21 10:05:56 -0400

    Update dice_rolling.py

--- dice_rolling.py
+++ dice_rolling.py
@@ -4,22 +4,19 @@ number_of_sides = input('How many sides does your dice have: ')
 num_list = list()
 roll = 'y'
 while True:
-    try:
-        roll = input('Do you want to roll the dice, Y or N: ')
-        roll = roll.lower()
-        if roll == "n":
-            print('Have a nice day!')
-            break
-        if roll != 'y':
-            print("Choose Y or N")
-            continue
-        if roll == "y":
-            dice_roll = random.randint(1,int(number_of_sides))
-            print(dice_roll)
-            num_list.append(dice_roll)
-    except:
-        print('Choose Y or N')
+    roll = input('Do you want to roll the dice, Y or N: ')
+    roll = roll.lower()
+    if roll == "n":
+        print('Have a nice day!')
+        break
+    elif roll != 'y':
+        print("Choose Y or N")
         continue
+    else:
+        dice_roll = random.randint(1,int(number_of_sides))
+        print(dice_roll)
+        num_list.append(dice_roll)
+
 
 x = sum(num_list)
 y = len(num_list)
